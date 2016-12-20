11769cce0793309894a1357b94a969f27335478a
Andrea Vitale <andrea@Andreas-MBP.homenet.telecomitalia.it>
2016-12-20 11:58:41 +0100

    Fixes #12

---
+++ ios/example/app.js
@@ -0,0 +1,63 @@
+var AvImageview = require("av.imageview");
+
+//Defining URLs
+var photos = [
+    "http://www.nationalgeographic.com/content/dam/photography/photos/000/060/6018.ngsversion.1467254523217.adapt.1900.1.jpg",
+    "http://www.italiangoodnews.com/wp-content/uploads/2014/11/italy-04.jpg",
+    "http://wp-admin.goldenbird-italy.com/wp-content/uploads/2015/07/italy_2631046a.jpg",
+    "http://www.travelviaitaly.com/wp-content/uploads/2015/12/Rome-Italy.jpg",
+    "http://an.example.of.broken.link.image/"
+];
+
+//and creating data source for listview
+var source = (function() {
+    var array = [];
+
+    photos.forEach(function(photo) {
+        array.push({
+            photo: {
+                image: photo
+            }
+        });
+    });
+
+    return array;
+})();
+
+//UI
+var default_template = {
+    properties: {
+        height: 160,
+    },
+    childTemplates: [{
+        type: 'AvImageview.ImageView',
+        bindId: 'photo',
+        properties: {
+            width: Ti.UI.FILL,
+            height: 160,
+            loadingIndicator: true,
+            defaultImage: "/placeholder.png",
+            brokenLinkImage: "/broken.png",
+            contentMode: AvImageview.CONTENT_MODE_ASPECT_FILL
+        }
+    }]
+};
+
+var window = Ti.UI.createWindow({
+    backgroundColor: "white"
+});
+
+var list_view = Ti.UI.createListView({
+    defaultItemTemplate: 'default',
+    templates: {
+        'default': default_template
+    }
+});
+
+var list_section = Ti.UI.createListSection();
+
+list_section.items = source;
+list_view.sections = [list_section];
+
+window.add(list_view);
+window.open();
