3d7a96f65a4409a1620ceff21307b522b43b668c
Andrea Vitale <vitale@everyup.it>
2018-05-17 08:35:38 +0200

    Release Android-4.0.0 and iOS-1.5.0

--- ios/iphone/Classes/AvImageviewModuleAssets.m
+++ ios/iphone/Classes/AvImageviewModuleAssets.m
@@ -7,18 +7,18 @@ extern NSData* filterDataInRange(NSData* thedata, NSRange range);
 
 @implementation AvImageviewModuleAssets
 
-- (NSData*) moduleAsset
+- (NSData *)moduleAsset
 {
-	
+  
 
-	return nil;
+  return nil;
 }
 
-- (NSData*) resolveModuleAsset:(NSString*)path
+- (NSData *)resolveModuleAsset:(NSString *)path
 {
-	
+  
 
-	return nil;
+  return nil;
 }
 
 @end
